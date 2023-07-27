5d34f8c53296f56ae6c1c17ebeda3529cbf21ea8
Madhan-Kumar <madhanrkv10@gmail.com>
2023-07-27 14:20:03 +0530

    Home Screen App Bar Added

--- app/src/main/java/com/example/trelloclone/activities/MainActivity.kt
+++ app/src/main/java/com/example/trelloclone/activities/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.trelloclone.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import android.view.View
+import android.widget.PopupMenu
 import com.example.trelloclone.R
 import com.example.trelloclone.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -23,5 +27,39 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, IntroActivity::class.java))
             finish()
         }
+
+        binding?.moreIcon?.setOnClickListener {
+
+
+            showPopupMenu(it)
+
+        }
+    }
+
+    private fun showPopupMenu(view: View) {
+        val popupMenu = PopupMenu(this, view)
+        popupMenu.inflate(R.menu.menu_popup)
+
+        // Set a listener to handle clicks on menu items
+        popupMenu.setOnMenuItemClickListener { menuItem: MenuItem ->
+            when (menuItem.itemId) {
+                R.id.menu_item_1 -> {
+                    Log.d("Selected", "first")
+                    // Handle click on Menu Item 1
+                    true
+                }
+
+                R.id.menu_item_2 -> {
+                    Log.d("Selected", "second")
+
+                    // Handle click on Menu Item 2
+                    true
+                }
+
+                else -> false
+            }
+        }
+
+        popupMenu.show()
     }
 }
--- app/src/main/java/com/example/trelloclone/activities/SplashScreen.kt
+++ app/src/main/java/com/example/trelloclone/activities/SplashScreen.kt
@@ -64,7 +64,6 @@ class SplashScreen : AppCompatActivity() {
             startActivity(Intent(this, IntroActivity::class.java))
             finish()
 
-
         }
     }
 }
